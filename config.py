b3ab3070c2706284e1ef0359605e79f0c398d091
Nícolas Oliveira <nicolasfilipeolvr@gmail.com>
2026-08-05 11:12:20 -0300

    Criar config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+TOKEN = os.getenv("TOKEN")  # Defina a variável de ambiente ou coloque o token diretamente
+ADMIN_CHAT_ID = -1001234567890  # ID do grupo/supergrupo onde o /paineladmin é permitido
+DATA_FILE = "data/config.json"

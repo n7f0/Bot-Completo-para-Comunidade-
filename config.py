a87141624f10b70346afb0dd4653252c1fc8abcd
Nícolas Oliveira <nicolasfilipeolvr@gmail.com>
2026-08-05 12:02:10 -0300

    Atualizar o config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 
-TOKEN = os.getenv("TOKEN")  # Defina a variável de ambiente ou coloque o token diretamente
-ADMIN_CHAT_ID = -1001234567890  # ID do grupo/supergrupo onde o /paineladmin é permitido
+TOKEN = os.getenv("DISCORD_TOKEN")
 DATA_FILE = "data/config.json"
